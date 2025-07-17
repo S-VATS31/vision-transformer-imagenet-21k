dc5a744eac8121a772de8fdd485467b0013dea08
Shaan Vats <shaanvats@Mac.lan>
2025-07-16 18:44:32 -0700

    changed import for gradscaler

--- training/training_components/setup_training_components.py
+++ training/training_components/setup_training_components.py
@@ -2,9 +2,9 @@ from typing import Tuple
 
 from configs.setup_env import device
 
-import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.amp import GradScaler
 
 from configs.training_args import TrainingArgs
 from training.training_components.cosine_scheduler import WarmupCosineScheduler
@@ -12,7 +12,7 @@ from training.training_components.cosine_scheduler import WarmupCosineScheduler
 def get_training_components(model: nn.Module, training_args: TrainingArgs) -> Tuple[
     optim.Optimizer, 
     optim.lr_scheduler.LambdaLR, 
-    torch.amp.GradScaler
+    GradScaler
 ]:
     """Set up optimizer, learning rate scheduler, and gradient scaler.
     
@@ -24,11 +24,11 @@ def get_training_components(model: nn.Module, training_args: TrainingArgs) -> Tu
         Tuple[
             optim.Optimizer, 
             optim.lr_scheduler.LambdaLR, 
-            torch.amp.GradScaler
+            GradScaler
         ]:
             - optim.Optimizer: AdamW optimizer.
             - optim.lr_scheduler.LambdaLR: Custom cosine warmup learning rate scheduler.
-            - torch.amp.GradScaler: Gradient scaling for bf16/fp16 gradients.
+            - GradScaler: Gradient scaling for bf16/fp16 gradients.
     """
     # Set up AdamW optimizer
     optimizer = optim.AdamW(
@@ -49,6 +49,6 @@ def get_training_components(model: nn.Module, training_args: TrainingArgs) -> Tu
     )
 
     # Set up GradScaler
-    scaler = torch.amp.GradScaler(device=device.type) if device.type == "cuda" else None
+    scaler = GradScaler(device=device.type) if device.type == "cuda" else None
 
     return optimizer, scheduler, scaler
