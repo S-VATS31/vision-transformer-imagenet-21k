cfb125cd5163507a54c300572334a11f05bd09cc
Shaan Vats <shaanvats@Mac.lan>
2025-07-17 00:43:34 -0700

    added logging

--- save_load_checkpoints/load_checkpoint.py
+++ save_load_checkpoints/load_checkpoint.py
@@ -1,11 +1,13 @@
 from typing import Optional, Dict, Union
+import logging
 
 import torch
 import torch.nn as nn
 from torch.amp import GradScaler
 
-# TODO: import logger
-# TODO: add logger name of ("checkpointing") or "checkpoint"
+# Set up logger
+from utils.setup_logger import setup_logger
+checkpoint_logger = setup_logger(name="checkpoint_logger", log_file="training.log", level=logging.INFO)
 
 def load_checkpoint(
     save_path: str,
@@ -18,7 +20,7 @@ def load_checkpoint(
     """Load checkpoint from saved .pt file.
     `
     Args:
-        filename (str): Filename where checkpoint is saved.
+        save_path (str): File path where checkpoint is saved.
         model (nn.Module): Transformer architecture.
         optimizer: PyTorch optimizer.
         scheduler: PyTorch scheduler.
@@ -43,13 +45,14 @@ def load_checkpoint(
         if scaler is not None and 'scaler_state_dict' in checkpoint:
             scaler.load_state_dict(checkpoint['scaler_state_dict'])
         
-        logger.info(f"Succesfully loaded checkpoint from {save_path}")
+        checkpoint_logger.info(f"Succesfully loaded checkpoint from {save_path}")
         
         return {
             'epoch': checkpoint['epoch'], # int 
             'loss': checkpoint['loss'], # float
         }
-        
+            
     except Exception as e:
-        logger.error(f"Failed to load checkpoint from {filename}: {e}")
+        checkpoint_logger.error(f"Failed to load checkpoint from {save_path}: {e}")
         raise
+    
